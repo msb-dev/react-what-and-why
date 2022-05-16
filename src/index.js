3cf136b0449fe0dfa288433006b49718ede08478
Matthew Brown <mail.mattbrown@gmail.com>
2022-05-16 15:43:58 +0100

    Get vanilla js version working apart from intentional bug

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
-function onSubmit(event) {
+function onAdd(event) {
   event.preventDefault();
-  const li = document.createElement('li')
-  const checkbox = document.createElement('input')
-  checkbox.setAttribute('type', )
+
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+
+  const span = document.createElement('span');
+  const textbox = document.getElementById('add-textbox');
+  span.innerText = textbox.value;
+  textbox.value = '';
+
+  const li = document.createElement('li');
+  li.appendChild(checkbox);
+  li.appendChild(span);
+
+  const ul = document.getElementById('list');
+  ul.appendChild(li);
+
+  const totalCountSpan = document.getElementById('total-count');
+  const newCount = parseInt(totalCountSpan.innerText) + 1;
+  totalCountSpan.innerText = newCount;
 }
 
 function attachSubmitHandlerToForm() {
   console.log('attachSubmitHandlerToForm called');
   const form = document.getElementById('add-todo');
-  form.addEventListener('submit', onSubmit);
+  form.addEventListener('submit', onAdd);
 }
 
-function onLooooad() {
+function onLoad() {
   console.log('onLooooad called');
   attachSubmitHandlerToForm();
 }
 
-document.addEventListener('load', onLooooad); // This actually doesn't work
+document.addEventListener('load', onLoad); // This actually doesn't work with react-scripts
 
 // This isn't usually needed, but Stackblitz is doing crazy magic that makes this required (and actually means that line 20 doesn't work)
 attachSubmitHandlerToForm();
