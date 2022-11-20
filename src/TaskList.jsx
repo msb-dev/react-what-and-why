f6722344e6fafa4e6d7688273ce6683d5c2ce1f7
Matthew Brown <mail.mattbrown@gmail.com>
2022-11-20 23:16:31 +0000

    Factor out Task list item into own component with props

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -3,33 +3,24 @@ import React from 'react';
 function TaskList() {
   return (
     <ul id="list">
-      <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" checked />
-        <span>Start to prepare presentation for Sheffield</span>
-        <button class="delete">Delete</button>
-      </li>
-      <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" checked />
-        <span>Realise it's way too hard</span>
-        <button class="delete">Delete</button>
-      </li>
-      <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" checked />
-        <span>PANIC!!!!!</span>
-        <button class="delete">Delete</button>
-      </li>
-      <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" />
-        <span>Give presentation</span>
-        <button class="delete">Delete</button>
-      </li>
-      <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" />
-        <span>Get banned from Sheffield Uni for ever</span>
-        <button class="delete">Delete</button>
-      </li>
+      <Task id="some-random-uuid-1" title="Start to prepare presentation for Sheffield" isComplete={true} />
+      <Task id="some-random-uuid-2" title="Realise it's way too hard" isComplete={true} />
+      <Task id="some-random-uuid-3" title="PANIC!!!!!" isComplete={true} />
+      <Task id="some-random-uuid-4" title="Give presentation" isComplete={false} />
+      <Task id="some-random-uuid-5" title="Get banned from Sheffield Uni for ever" isComplete={false} />
     </ul>
   );
 }
+
+function Task({id, title, isComplete}) {
+  return (
+   <li id={id}>
+     <input type="checkbox" class="checkbox" checked={isComplete} />
+     <span>{title}</span>
+     <button class="delete">Delete</button>
+   </li>
+  )
+}
+
 
 export default TaskList;
