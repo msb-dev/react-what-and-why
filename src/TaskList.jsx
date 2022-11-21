95e58d60e8de154ce767c101e58e4cf7e0da6ee3
Matthew Brown <mail.mattbrown@gmail.com>
2022-11-21 01:19:11 +0000

    Change class to classname

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -4,29 +4,29 @@ function TaskList() {
   return (
     <ul id="list">
       <li id="some-random-uuid-1">
-        <input type="checkbox" class="checkbox" checked />
+        <input type="checkbox" className="checkbox" checked />
         <span>Start to prepare presentation for Sheffield</span>
-        <button class="delete">Delete</button>
+        <button className="delete">Delete</button>
       </li>
       <li id="some-random-uuid-2">
-        <input type="checkbox" class="checkbox" checked />
+        <input type="checkbox" className="checkbox" checked />
         <span>Realise it's way too hard</span>
-        <button class="delete">Delete</button>
+        <button className="delete">Delete</button>
       </li>
       <li id="some-random-uuid-3">
-        <input type="checkbox" class="checkbox" checked />
+        <input type="checkbox" className="checkbox" checked />
         <span>PANIC!!!!!</span>
-        <button class="delete">Delete</button>
+        <button className="delete">Delete</button>
       </li>
       <li id="some-random-uuid-4">
-        <input type="checkbox" class="checkbox" />
+        <input type="checkbox" className="checkbox" />
         <span>Give presentation</span>
-        <button class="delete">Delete</button>
+        <button className="delete">Delete</button>
       </li>
       <li id="some-random-uuid-5">
-        <input type="checkbox" class="checkbox" />
+        <input type="checkbox" className="checkbox" />
         <span>Get banned from Sheffield Uni for ever</span>
-        <button class="delete">Delete</button>
+        <button className="delete">Delete</button>
       </li>
     </ul>
   );
