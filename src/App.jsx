a2875918d3843e88593cfbacebb19bdc48ee2c55
Matthew Brown <matt.brown@cambridgeconsultants.com>
2022-11-22 22:18:37 +0000

    Factor out TaskList into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+
+import TaskList from "./TaskList";
 
 function App() {
   return (
@@ -28,33 +30,7 @@ function App() {
           Completed <span id="completed-count">3</span> out of
           <span id="total-count"> 5</span>
         </p>
-        <ul id="list">
-          <li id="some-random-uuid-1">
-            <input type="checkbox" className="checkbox" checked />
-            <span>Start to prepare presentation for Sheffield</span>
-            <button className="delete">Delete</button>
-          </li>
-          <li id="some-random-uuid-1">
-            <input type="checkbox" className="checkbox" checked />
-            <span>Realise it's way too hard</span>
-            <button className="delete">Delete</button>
-          </li>
-          <li id="some-random-uuid-1">
-            <input type="checkbox" className="checkbox" checked />
-            <span>PANIC!!!!!</span>
-            <button className="delete">Delete</button>
-          </li>
-          <li id="some-random-uuid-1">
-            <input type="checkbox" className="checkbox" />
-            <span>Give presentation</span>
-            <button className="delete">Delete</button>
-          </li>
-          <li id="some-random-uuid-1">
-            <input type="checkbox" className="checkbox" />
-            <span>Get banned from Sheffield Uni for ever</span>
-            <button className="delete">Delete</button>
-          </li>
-        </ul>
+        <TaskList />
         <form id="add-todo">
           <input id="add-textbox" />
           <button id="add-button">Add</button>
---
+++ src/TaskList.jsx
@@ -0,0 +1,33 @@
+function TaskList() {
+  return (
+    <ul id="list">
+      <li id="some-random-uuid-1">
+        <input type="checkbox" className="checkbox" checked />
+        <span>Start to prepare presentation for Sheffield</span>
+        <button className="delete">Delete</button>
+      </li>
+      <li id="some-random-uuid-2">
+        <input type="checkbox" className="checkbox" checked />
+        <span>Realise it's way too hard</span>
+        <button className="delete">Delete</button>
+      </li>
+      <li id="some-random-uuid-3">
+        <input type="checkbox" className="checkbox" checked />
+        <span>PANIC!!!!!</span>
+        <button className="delete">Delete</button>
+      </li>
+      <li id="some-random-uuid-4">
+        <input type="checkbox" className="checkbox" />
+        <span>Give presentation</span>
+        <button className="delete">Delete</button>
+      </li>
+      <li id="some-random-uuid-5">
+        <input type="checkbox" className="checkbox" />
+        <span>Get banned from Sheffield Uni for ever</span>
+        <button className="delete">Delete</button>
+      </li>
+    </ul>
+  );
+}
+
+export default TaskList;
