a7070ecb430ef41ddbcb6e8fd18e58447598b79a
Phoebe Pinney <93838460+PhoebePinney@users.noreply.github.com>
2024-11-26 17:00:53 +0000

    Fix logo and update uni references

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 function TaskList() {
   const [tasks, setTasks] = useState([
-    {id: "some-random-uuid-1", title: "Start to prepare presentation for Sheffield", isComplete: true},
+    {id: "some-random-uuid-1", title: "Start to prepare presentation for Manchester", isComplete: true},
     {id: "some-random-uuid-2", title: "Realise it's way too hard", isComplete: true},
     {id: "some-random-uuid-3", title: "PANIC!!!!!", isComplete: true},
     {id: "some-random-uuid-4", title: "Give presentation", isComplete: false},
-    {id: "some-random-uuid-5", title: "Get banned from Sheffield Uni for ever", isComplete: false},
+    {id: "some-random-uuid-5", title: "Get banned from UoM forever", isComplete: false},
   ])
 
   return (
